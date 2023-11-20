668861b954e652d107c63a9761e559b38d18aaf7
Georgina <66384787+GeorginaMampuru@users.noreply.github.com>
2023-11-20 19:15:07 +0200

    HubRes

---
+++ backend/config/passport.js
@@ -0,0 +1,24 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "12@eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5ODI0ODgwNCwiaWF0IjoxNjk4MjQ4ODA0fQ.LVhUtxfglyxYdNvaXzmaGPxFgcQNXKQ_GWmihGRbD1o";
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
